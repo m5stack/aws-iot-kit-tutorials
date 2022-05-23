41f4135fbdf08b3ccae5da232102de2e690ee2e5
Rashed Talukder <9218468+rashedtalukder@users.noreply.github.com>
2022-05-23 13:57:15 -0700

    Update jquery for v1 api ref

--- workshop/static/en/api-reference/search/functions_8.js
+++ workshop/static/en/api-reference/search/functions_8.js
@@ -3,5 +3,5 @@ var searchData=
   ['speaker_5fdeinit_0',['Speaker_Deinit',['../speaker_8c.html#a6964a2985bad4f4f8c1a57f3feebbcf4',1,'Speaker_Deinit():&#160;speaker.c'],['../speaker_8h.html#a6964a2985bad4f4f8c1a57f3feebbcf4',1,'Speaker_Deinit():&#160;speaker.c']]],
   ['speaker_5finit_1',['Speaker_Init',['../speaker_8c.html#aabdbd6c7828802c26f42d05bafe96f53',1,'Speaker_Init():&#160;speaker.c'],['../speaker_8h.html#aabdbd6c7828802c26f42d05bafe96f53',1,'Speaker_Init():&#160;speaker.c']]],
   ['speaker_5fwritebuff_2',['Speaker_WriteBuff',['../speaker_8c.html#a53db8ae233bb5adfd464cb01a8549e22',1,'Speaker_WriteBuff(uint8_t *buff, uint32_t len, uint32_t timeout):&#160;speaker.c'],['../speaker_8h.html#a53db8ae233bb5adfd464cb01a8549e22',1,'Speaker_WriteBuff(uint8_t *buff, uint32_t len, uint32_t timeout):&#160;speaker.c']]],
-  ['spi_5fpoll_3',['spi_poll',['../disp__spi_8c.html#a3dc6a7525e5a0b34e37a3a64478ec9ee',1,'spi_poll():&#160;disp_spi.c'],['../disp__spi_8h.html#a3dc6a7525e5a0b34e37a3a64478ec9ee',1,'spi_poll():&#160;disp_spi.c']]]
+  ['spi_5fpoll_3',['spi_poll',['../disp__spi_8h.html#a3dc6a7525e5a0b34e37a3a64478ec9ee',1,'spi_poll():&#160;disp_spi.c'],['../disp__spi_8c.html#a3dc6a7525e5a0b34e37a3a64478ec9ee',1,'spi_poll():&#160;disp_spi.c']]]
 ];
